1ea8581da41492d61d3e1541c0944345d995d6b5
Mokairui <827858446@qq.com>
2023-09-18 23:11:03 +0800

    byte-buddy

--- bytecode-bucket/sunshine-byte-buddy/src/main/java/com/sunshine/GeneratorClazzMethod.java
+++ bytecode-bucket/sunshine-byte-buddy/src/main/java/com/sunshine/GeneratorClazzMethod.java
@@ -1,11 +1,13 @@
 package com.sunshine;
 
 import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.implementation.MethodDelegation;
 import net.bytebuddy.matcher.ElementMatchers;
 
-import java.io.FileOutputStream;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
 
 /**
  * @author Mokairui
@@ -15,15 +17,29 @@ import java.lang.reflect.InvocationTargetException;
 public class GeneratorClazzMethod {
 
     public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        Class<?> dynamicType = new ByteBuddy().subclass(Object.class)
+        DynamicType.Unloaded<Object> dynamicClazz = new ByteBuddy().subclass(Object.class)
                 .method(ElementMatchers.named("toString"))
                 .intercept(FixedValue.value("Hello World!"))
-                .make()
-                .load(GeneratorClazzMethod.class.getClassLoader())
-                .getLoaded();
+                .make();
+        Class<?> dynamicType = dynamicClazz.load(GeneratorClazzMethod.class.getClassLoader()).getLoaded();
 
         String msg = dynamicType.getDeclaredConstructor().newInstance().toString();
         System.out.println(msg);
+
+
+        DynamicType.Unloaded<Object> clazzLoad = new ByteBuddy().subclass(Object.class)
+                .defineMethod("main", void.class, Modifier.PUBLIC + Modifier.STATIC)
+                .withParameter(String[].class, "args")
+                .intercept(MethodDelegation.to(Hi.class))
+                .make();
+        Class<?> clazz = clazzLoad.load(GeneratorClazzMethod.class.getClassLoader()).getLoaded();
+        clazz.getMethod("main", String[].class).invoke(clazz.getDeclaredConstructor().newInstance(), (Object) new String[1]);
+    }
+    
+    public static class Hi {
+        public static void main(String[] args) {
+            System.out.println("helloWorld");
+        }
     }
     
 }
