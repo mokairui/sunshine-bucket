69c694e264f3cf431ec3da2f80c5b688dec60f64
Mokairui <827858446@qq.com>
2023-09-18 23:24:38 +0800

    byte-buddy

--- bytecode-bucket/sunshine-byte-buddy/src/main/java/com/sunshine/GeneratorClazzMethod.java
+++ bytecode-bucket/sunshine-byte-buddy/src/main/java/com/sunshine/GeneratorClazzMethod.java
@@ -16,8 +16,27 @@ import java.lang.reflect.Modifier;
  */
 public class GeneratorClazzMethod {
 
-    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) throws Exception {
+        // 生成类信息
+        createClazz();
+        
+        // 覆盖 toString 方法
+        createMainMethod();
+        
+        // 委托函数使用
+        delegateProcedureMethod();
+    }
+
+    private static void createClazz() {
+        // 这样会在编译时 生成一个 com.sunshine.HelloByteBuddy 字节码文件
+        DynamicType.Unloaded<Object> dynamicType = new ByteBuddy()
+                .subclass(Object.class).name("com.sunshine.HelloByteBuddy")
+                .make();
+    }
+
+    private static void createMainMethod() throws Exception {
         DynamicType.Unloaded<Object> dynamicClazz = new ByteBuddy().subclass(Object.class)
+                .name("com.sunshine.HelloByteBuddy2")
                 .method(ElementMatchers.named("toString"))
                 .intercept(FixedValue.value("Hello World!"))
                 .make();
@@ -25,17 +44,20 @@ public class GeneratorClazzMethod {
 
         String msg = dynamicType.getDeclaredConstructor().newInstance().toString();
         System.out.println(msg);
-
-
+    }
+    
+    private static void delegateProcedureMethod() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         DynamicType.Unloaded<Object> clazzLoad = new ByteBuddy().subclass(Object.class)
+                .name("com.sunshine.HelloByteBuddy3")
                 .defineMethod("main", void.class, Modifier.PUBLIC + Modifier.STATIC)
                 .withParameter(String[].class, "args")
+                // MethodDelegation 方法委托 但是需要方法的 入参和出参是一样才可以映射上
                 .intercept(MethodDelegation.to(Hi.class))
                 .make();
         Class<?> clazz = clazzLoad.load(GeneratorClazzMethod.class.getClassLoader()).getLoaded();
         clazz.getMethod("main", String[].class).invoke(clazz.getDeclaredConstructor().newInstance(), (Object) new String[1]);
     }
-    
+
     public static class Hi {
         public static void main(String[] args) {
             System.out.println("helloWorld");
