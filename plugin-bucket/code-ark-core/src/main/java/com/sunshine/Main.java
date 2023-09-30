2f8f3d05b6e76e3b37d679838ff3347bc3bb6836
Mokairui <827858446@qq.com>
2023-09-30 23:06:57 +0800

    idea插件字节码插件开发项目初始化

---
+++ plugin-bucket/code-ark-core/src/main/java/com/sunshine/Main.java
@@ -0,0 +1,12 @@
+package com.sunshine;
+
+/**
+ * @author Mokairui
+ * @description
+ * @since 2023/9/25
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
