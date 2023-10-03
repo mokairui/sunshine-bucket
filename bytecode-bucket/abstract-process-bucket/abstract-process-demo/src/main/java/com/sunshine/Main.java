b8fac9e59b8dfa2a569bef955cf8b1d3837c23ce
Mokairui <827858446@qq.com>
2023-10-04 01:00:02 +0800

    AbstractProcessor调式

---
+++ bytecode-bucket/abstract-process-bucket/abstract-process-demo/src/main/java/com/sunshine/Main.java
@@ -0,0 +1,16 @@
+package com.sunshine;
+
+/**
+ * @author Mokairui
+ * @description
+ * @since 2023/10/2
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        MethodMonitor methodMonitor = new MethodMonitor();
+        methodMonitor.echoHi();
+        System.out.println("Hello world!");
+    }
+
+}
