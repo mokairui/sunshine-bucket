16555e60b5680c49dcb83bb99a11374b22b20637
Mokairui <827858446@qq.com>
2023-11-01 23:34:35 +0800

    bridge init

---
+++ design-pattern-bucket/structural-bucket/bridge-pattern/src/main/java/com/sunshine/infrastructure/House.java
@@ -0,0 +1,18 @@
+package com.sunshine.infrastructure;
+
+/**
+ * @author Mokairui
+ * @description 房地产
+ * @since 2023/11/1
+ */
+public class House extends Product {
+    @Override
+    public void beProducted() {
+        System.out.println("生产出的房子是这个样子的...");
+    }
+
+    @Override
+    public void beSelled() {
+        System.out.println("生产出的房子卖出去了...");
+    }
+}
---
+++ design-pattern-bucket/structural-bucket/bridge-pattern/src/main/java/com/sunshine/infrastructure/Product.java
@@ -0,0 +1,16 @@
+package com.sunshine.infrastructure;
+
+/**
+ * @author Mokairui
+ * @description
+ * @since 2023/11/1
+ */
+public abstract class Product {
+
+    //甭管是什么产品它总要是能被生产出来
+    public abstract void beProducted();
+
+    //生产出来的东西，一定要销售出去，否则扩本呀
+    public abstract void beSelled();
+
+}
